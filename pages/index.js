b8d6a24eb92259db0128fb27e3fe0790a2d00bea
apinanyogaratnam <apinanapinan@icloud.com>
2023-01-10 11:05:01 -0500

    feat: created nav bar template

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,33 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+const NavBar = () => (
+  <div style={{
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: 'lightblue',
+    borderRadius: '20px',
+    width: '95%',
+    margin: '0 auto',
+    marginTop: '10px',
+  }}>
+    <h3>Home</h3>
+    <h3>Portfolio</h3>
+    <h3>About</h3>
+    <h3>Contact</h3>
+  </div>
+);
+
 export default function Home() {
   return (
-    <div>
-      Apinan Yogaratnam
-    </div>
+    <main>
+      <Head>
+
+      </Head>
+      <div>
+        <NavBar />
+      </div>
+    </main>
   )
 }
