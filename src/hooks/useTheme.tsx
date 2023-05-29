99b9aef45ea6fb7616c7e8a48ebea0df0d591a51
apinanyogaratnam <apinan@ontopical.com>
2023-05-29 10:26:07 -0400

    feat: navbar

---
+++ src/hooks/useTheme.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import { getLocalStorage, setLocalStorage } from '@/utils/storage';
+
+export enum Theme {
+    LIGHT = 'light',
+    DARK = 'dark',
+}
+
+const useTheme = () => {
+    const [hasMounted, setHasMounted] = useState(false);
+    const currentTheme = (Theme.LIGHT) as Theme;
+    const [theme, setTheme] = useState<Theme>(currentTheme);
+    const [oppositeTheme, setOppositeTheme] = useState<Theme>(currentTheme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
+
+    useEffect(() => {
+        setHasMounted(true);
+    }, []);
+
+    useEffect(() => {
+        if (hasMounted) {
+            setOppositeTheme(theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
+        }
+    }, [theme, hasMounted]);
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+          document.documentElement.className = theme;
+        }
+      }, [theme]);
+
+    const toggleTheme = () => {
+        if (!hasMounted) {
+            return;
+        }
+        const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
+
+        setTheme(newTheme);
+        setLocalStorage('theme', newTheme);
+    }
+
+    if (!hasMounted) {
+        return { theme: Theme.LIGHT, toggleTheme, oppositeTheme: Theme.DARK };
+    }
+
+    return { theme, toggleTheme, oppositeTheme };
+}
+
+export default useTheme;
---
+++ src/utils/storage.ts
@@ -0,0 +1,15 @@
+const getLocalStorage = (key: string): string | null => {
+    if (typeof window === 'undefined') {
+        return null;
+    }
+    return localStorage.getItem(key);
+};
+
+const setLocalStorage = (key: string, value: string) => {
+    if (typeof window === 'undefined') {
+        return;
+    }
+    localStorage.setItem(key, value);
+};
+
+export { getLocalStorage, setLocalStorage };
