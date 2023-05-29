044292665306f7f554f1dcb0f6d17cee4981ffb3
apinanyogaratnam <apinan@ontopical.com>
2023-05-29 18:02:01 -0400

    style(refactor): styling changes for main content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,42 +61,51 @@ const NavBar: React.FC = () => {
 
 const MainContent: React.FC = () => {
     return (
-        <div className="mt-20 flex flex-row items-center justify-center space-x-4">
-            <div className="relative w-[27%] border-4 border-black dark:border-white" style={{ height: "400px" }}>
+        <div className="mt-20 flex flex-row justify-center space-x-4">
+            <div
+                className="relative hidden w-[40%] border-4 border-black dark:border-white md:block"
+                style={{ height: "600px" }}
+            >
                 <div className="h-full overflow-hidden">
                     <Image
                         src="/headshot.jpeg"
                         alt="Apinan Yogaratnam"
                         fill
-                        className="absolute z-0"
+                        // className="absolute z-0"
                         style={{
                             objectFit: "cover",
                         }}
                     />
                 </div>
             </div>
-            <div className="w-[27%]">
-                <h1 className="text-justify text-6xl font-extrabold text-[hsl(280,100%,70%)]">
-                    Bridging Imagination and Reality Through Code
+            <div className="w-[35%] flex flex-col justify-between p-2">
+                <h1 className="text-left text-7xl font-extrabold text-[hsl(280,100%,70%)]">
+                    Turning Vision Into Reality With Code And Design.
                 </h1>
-                <p className="text-black dark:text-white">
+                <p className="text-xl text-black dark:text-white">
                     As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
                     Explore my latest projects, showcasing my expertise in web development.
                 </p>
-                <button
-                    className="mt-4 flex flex-row rounded-md bg-black px-4 py-2 text-white dark:bg-white dark:text-black"
-                    onClick={() =>
-                        window.open(
-                            "https://drive.google.com/file/d/12A4kIJ4ebThgixk10x_iJrSWfyiVP0Es/view?usp=sharing",
-                            "_blank"
-                        )
-                    }
-                >
-                    Resume
-                    <span className="transition-colors duration-0">
-                        <MdOutlineOpenInNew size="1.5em" className="" />
-                    </span>
-                </button>
+                <div className="flex flex-row items-center justify-start space-x-4">
+                    <button
+                        className="mt-4 flex flex-row rounded-md bg-black px-4 py-2 text-white dark:bg-white dark:text-black"
+                        onClick={() =>
+                            window.open(
+                                "https://drive.google.com/file/d/12A4kIJ4ebThgixk10x_iJrSWfyiVP0Es/view?usp=sharing",
+                                "_blank"
+                            )
+                        }
+                    >
+                        Resume
+                        <span className="transition-colors duration-0">
+                            {/* TODO: use different icons and try */}
+                            <MdOutlineOpenInNew size="1.5em" className="" />
+                        </span>
+                    </button>
+                    <button className="mt-2">
+                        <p className="text-black underline dark:text-white">Contact</p>
+                    </button>
+                </div>
             </div>
         </div>
     );
