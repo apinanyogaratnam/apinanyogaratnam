9b35b64eb841da8fba7ae688b854d3dff9a0c24e
apinanyogaratnam <apinan@ontopical.com>
2023-06-03 10:08:54 -0400

    feat: progress

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,6 +3,11 @@ import { RxHamburgerMenu } from "react-icons/rx";
 
 import useTheme, { Theme } from "@/hooks/useTheme";
 
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import { BsStackOverflow } from "react-icons/bs";
+import { FiTwitter } from "react-icons/fi";
+import { FaStackOverflow } from "react-icons/fa";
+
 const NavBar: React.FC = () => {
     const { theme, toggleTheme, oppositeTheme } = useTheme();
 
@@ -50,7 +55,29 @@ const NavBar: React.FC = () => {
             </div>
             <div className="w-[33%]">
                 <div className="hidden flex-row items-center justify-center md:flex">
-                    <h1 className="text-black dark:text-white">Social Links</h1>
+                    <div className="flex flex-row space-x-2 text-black dark:text-white">
+                        <Link
+                            href="https://www.linkedin.com/in/apinanyogaratnam"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <AiFillLinkedin size="1.5em" className="h-8 w-8 rounded-full bg-blue-500 p-1" />
+                        </Link>
+                        <Link
+                            className="rounded-full bg-blue-500 p-1"
+                            href="https://stackoverflow.com/users/13776723/apinanyogaratnam"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FaStackOverflow size="1.5em" className="" />
+                        </Link>
+                        <Link href="https://twitter.com/apiyogaratnam">
+                            <FiTwitter size="1.5em" className="h-8 w-8 rounded-full bg-blue-400 p-1" />
+                        </Link>
+                        <Link href="https://github.com/apinanyogaratnam">
+                            <AiFillGithub size="1.5em" className="h-8 w-8 rounded-full bg-black p-1" />
+                        </Link>
+                    </div>
                     <div className="ml-2 mt-2">
                         <label className="relative inline-flex cursor-pointer items-center">
                             <input
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { BsArrowUpCircle } from "react-icons/bs";
+import { BsArrowDownCircle, BsArrowUpCircle } from "react-icons/bs";
 
 import About from "@/components/about";
 import Experience from "@/components/experience";
@@ -8,7 +8,33 @@ import MainContent from "@/components/main-content";
 import NavBar from "@/components/navbar";
 import Projects from "@/components/projects";
 
+const scrollToElement = (elementId) => {
+    const element = document.getElementById(elementId);
+
+    if (element) {
+        const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
+
+        // Scroll smoothly to the element
+        window.scrollTo({ top: elementPosition, behavior: "smooth" });
+    }
+};
+
 const Home: NextPage = () => {
+    const [currentSection, setCurrentSection] = useState(0);
+    const sectionIds = ["main-content", "experience", "projects", "about"];
+
+    const scrollToNextSection = () => {
+        if (currentSection < sectionIds.length - 1) {
+            const nextSectionId = sectionIds[currentSection + 1];
+            const nextSection = document.getElementById(nextSectionId);
+
+            if (nextSection) {
+                const nextSectionPosition = nextSection.getBoundingClientRect().top + window.pageYOffset;
+                window.scrollTo({ top: nextSectionPosition, behavior: "smooth" });
+                setCurrentSection(currentSection + 1);
+            }
+        }
+    };
     return (
         <>
             <Head>
@@ -17,7 +43,14 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className="ts bg-white p-2 dark:bg-gray-900">
-                <BsArrowUpCircle className="fixed bottom-5 right-5 cursor-pointer text-4xl text-blue-500 dark:text-blue-300 hover:text-blue-700 dark:hover:text-blue-500" onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })} />
+                <BsArrowUpCircle
+                    className="fixed bottom-5 right-5 cursor-pointer text-4xl text-blue-500 hover:text-blue-700 dark:text-blue-300 dark:hover:text-blue-500"
+                    onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+                />
+                <BsArrowDownCircle
+                    className="fixed bottom-5 right-16 cursor-pointer text-4xl text-blue-500 hover:text-blue-700 dark:text-blue-300 dark:hover:text-blue-500"
+                    onClick={() => window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" })}
+                />
                 <NavBar />
                 <MainContent />
                 <Experience />
