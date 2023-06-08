73463c0269ea14b2e5ea81c61ed7682386d636df
apinanyogaratnam <apinan@ontopical.com>
2023-06-08 15:54:02 -0400

    refactor: consistency

--- next.config.mjs
+++ next.config.mjs
@@ -22,7 +22,7 @@ const config = {
         if (!dev && !isServer) {
             Object.assign(config.resolve.alias, {
                 "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-                react: "preact/compat",
+                "react": "preact/compat",
                 "react-dom/test-utils": "preact/test-utils",
                 "react-dom": "preact/compat",
             });
