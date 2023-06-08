2beaeb989069e243eaf22e7df733da21c37ced8f
apinanyogaratnam <apinan@ontopical.com>
2023-06-08 09:05:54 -0400

    refactor: updated projects page

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -6,18 +6,14 @@ const Projects: React.FC = () => {
             <h1 className="text-center text-5xl text-black dark:text-white">Projects</h1>
             <div className="mt-10 w-full grid-cols-3 md:grid">
                 {[
-                    { name: "Ontopical", type: "personal", website: "https://example.com" },
-                    { name: "One Cart Home", type: "freelance", website: "https://example.com" },
-                    { name: "SAP Fioneer", type: "personal", website: "https://example.com" },
+                    { name: "Api Key Management System", type: "personal", website: "", github: "https://github.com/apinanyogaratnam/akms" },
                     { name: "AI3", type: "freelance", website: "https://ai3-client-git-main-ai3coadmin.vercel.app" },
                 ].map((company, index) => (
                     <div className="p-2" key={index}>
                         <div className="flex w-full flex-col justify-between rounded-lg border-2 border-inherit p-2 text-center">
                             <h1 className="text-2xl font-bold text-black dark:text-white">{company.name}</h1>
-                            <p className="text-black dark:text-white">Logo goes here</p>
+                            <p className="text-black dark:text-white">Image goes here</p>
                             <p className="text-lg text-black dark:text-white">{company.type}</p>
-                            <p className="text-lg text-black dark:text-white">Feb 2023 - Present</p>
-                            <p className="text-lg text-black dark:text-white">Software Engineer</p>
                             <p className="text-lg text-black dark:text-white">
                                 Python, React, Typescript, Nextjs, Postgres, Docker, AWS, Elasticsearch
                             </p>
