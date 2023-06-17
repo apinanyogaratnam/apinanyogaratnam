11d71248e8861b2f96d2a476cd1128f015796cc6
apinanyogaratnam <apinan@ontopical.com>
2023-06-17 11:39:57 -0400

    style: formatting code

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import { Button } from "@/components";
-import Link from "next/link";
 
 const Projects: React.FC = () => {
     return (
