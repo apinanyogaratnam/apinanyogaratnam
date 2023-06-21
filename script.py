8e2f7bc4c29e83eff4283cb1043d4f2a70c69227
apinanyogaratnam <apinan@ontopical.com>
2023-06-20 22:02:16 -0400

    style: formatted script.py

--- script.py
+++ script.py
@@ -2,11 +2,11 @@ import os
 import time
 from datetime import datetime
 
-list_of_files_to_ignore = ['node_modules', '.next', 'public', '.git']
+list_of_files_to_ignore = ["node_modules", ".next", "public", ".git"]
+
 
 def get_all_files_in_current_directory_recursively(
-    directory: str,
-    list_of_files_to_ignore: list = []
+    directory: str, list_of_files_to_ignore: list = []
 ) -> list:
     all_files = []
     for root, dirs, files in os.walk(directory):
@@ -18,22 +18,32 @@ def get_all_files_in_current_directory_recursively(
 
 
 file_contents = {}
-for file in get_all_files_in_current_directory_recursively(os.getcwd(), list_of_files_to_ignore):
-    with open(file, 'r') as f:
+for file in get_all_files_in_current_directory_recursively(
+    os.getcwd(), list_of_files_to_ignore
+):
+    with open(file, "r") as f:
         file_contents[file] = f.read()
 
 while True:
-    for file in get_all_files_in_current_directory_recursively(os.getcwd(), list_of_files_to_ignore):
-        with open(file, 'r') as f:
+    for file in get_all_files_in_current_directory_recursively(
+        os.getcwd(), list_of_files_to_ignore
+    ):
+        with open(file, "r") as f:
             current_content = f.read()
             if file_contents.get(file) != current_content:
                 file_contents[file] = current_content
-                with open('public/sitemap.xml', 'r') as sitemap:
+                with open("public/sitemap.xml", "r") as sitemap:
                     sitemap_contents = sitemap.read()
-                index_of_lastmod = sitemap_contents.find('<lastmod>')
-                index_of_lastmod_end = sitemap_contents.find('</lastmod>', index_of_lastmod)
+                index_of_lastmod = sitemap_contents.find("<lastmod>")
+                index_of_lastmod_end = sitemap_contents.find(
+                    "</lastmod>", index_of_lastmod
+                )
                 if index_of_lastmod != -1 and index_of_lastmod_end != -1:
-                    sitemap_contents = sitemap_contents[:index_of_lastmod + 9] + datetime.now().strftime('%Y-%m-%d') + sitemap_contents[index_of_lastmod_end:]
-                    with open('public/sitemap.xml', 'w') as sitemap:
+                    sitemap_contents = (
+                        sitemap_contents[: index_of_lastmod + 9]
+                        + datetime.now().strftime("%Y-%m-%d")
+                        + sitemap_contents[index_of_lastmod_end:]
+                    )
+                    with open("public/sitemap.xml", "w") as sitemap:
                         sitemap.write(sitemap_contents)
     time.sleep(5)
