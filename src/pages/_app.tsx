4606bc38af5da8fa5ebbe16d3e9500c2d377385f
Apinan Yogaratnam <apinanapinan@icloud.com>
2024-12-19 20:30:17 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+
 import { useRouter } from "next/router";
 
 import { api } from "@/utils/api";
