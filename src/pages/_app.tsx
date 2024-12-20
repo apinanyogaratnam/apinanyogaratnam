125bf31f9a2180d91d4386c91e06b5f0b13202ee
Apinan Yogaratnam <apinanapinan@icloud.com>
2024-12-19 20:29:22 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,23 @@ import { Analytics } from "@vercel/analytics/react";
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { useRouter } from "next/router";
 
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
+    const router = useRouter();
+
+    const route = async () => {
+        await router.push("https://career-portfolio-lemon.vercel.app");
+    };
+
+    useEffect(() => {
+        route();
+    }, [route]);
+
     return (
         <SessionProvider session={session}>
             <Component {...pageProps} />
